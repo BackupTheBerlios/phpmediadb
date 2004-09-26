ce3fd4a093896434d8016a58f777212a4db4b8f3
mblaschke <mblaschke>
2004-09-26 16:00:06 +0000

    * Added basic includes for all tiers * Added all includes for tier.presentation

--- phpmediadb-cvs/_source/phpmediadb.php
+++ phpmediadb-cvs/_source/phpmediadb.php
@@ -1,6 +1,24 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: phpmediadb.php,v 1.1 2004/09/26 15:03:27 mblaschke Exp $ */
+/* $Id: phpmediadb.php,v 1.2 2004/09/26 16:00:06 mblaschke Exp $ */
+
+/* include tier.global */
+require_once( 'tier.global/class.phpmediadb.php' );
+require_once( 'tier.global/constants.phpmediadb.php' );
+
+/* include tier.presentation */
+require_once( 'tier.presentation/class.phpmediadb_presentation.php' );
+require_once( 'tier.presentation/class.phpmediadb_presentation_contentvars.php' );
+require_once( 'tier.presentation/class.phpmediadb_presentation_htmlservice.php' );
+require_once( 'tier.presentation/class.phpmediadb_presentation_i18n.php' );
+require_once( 'tier.presentation/class.phpmediadb_presentation_session.php' );
+require_once( 'tier.presentation/class.phpmediadb_presentation_xmlservice.php' );
+
+/* include tier.business */
+require_once( 'tier.business/class.phpmediadb_business.php' );
+
+/* include tier.data */
+require_once( 'tier.data/class.phpmediadb_data.php' );
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
