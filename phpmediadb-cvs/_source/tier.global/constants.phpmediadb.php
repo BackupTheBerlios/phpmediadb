f9faedc85fb1e451494f362835f06fa3a60d9954
mblaschke <mblaschke>
2004-09-26 16:08:27 +0000

    * Added 2 Constants needed by class phpmediadb_presentation_contentvars

--- phpmediadb-cvs/_source/tier.global/constants.phpmediadb.php
+++ phpmediadb-cvs/_source/tier.global/constants.phpmediadb.php
@@ -1,6 +1,21 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: constants.phpmediadb.php,v 1.1 2004/09/26 15:03:27 mblaschke Exp $ */
+/* $Id: constants.phpmediadb.php,v 1.2 2004/09/26 16:08:27 mblaschke Exp $ */
+
+// --- tier.presentation ---
+	/*
+	 * PHPMEDIADB_NODEFORMAT_HTML:
+	 *   Value is HTML and need no conversion
+	 *
+	 * PHPMEDIADB_NODEFORMAT_TEXT:
+	 *   Value is Plaintext and need conversion to XML rules
+	 */
+	define("PHPMEDIADB_NODEFORMAT_HTML", 1);
+	define("PHPMEDIADB_NODEFORMAT_TEXT", 0);
+
+// --- tier.business ---
+
+// --- tier.data ---
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
