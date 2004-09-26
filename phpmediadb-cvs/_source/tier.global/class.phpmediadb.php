6cf015849865be3f22137b907b9b21e17ee6cf17
mblaschke <mblaschke>
2004-09-26 16:09:34 +0000

    * Added basic class * Implemented all attributes and constructor

--- phpmediadb-cvs/_source/tier.global/class.phpmediadb.php
+++ phpmediadb-cvs/_source/tier.global/class.phpmediadb.php
@@ -1,6 +1,81 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: class.phpmediadb.php,v 1.1 2004/09/26 15:03:27 mblaschke Exp $ */
+/* $Id: class.phpmediadb.php,v 1.2 2004/09/26 16:09:34 mblaschke Exp $ */
+
+/**
+ * Main class of the phpMediaDB Project. It contains all other classes in the
+ * PRESENTATION, BUSINESS and DATA.
+ *
+ * @access public
+ * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+ * @package phpmediadb
+ * @since 0.0.0
+ * @subpackage global
+ * @version 1.0.0
+ */
+class phpmediadb
+{
+	// --- ATTRIBUTES ---
+
+	/**
+	 * Container for phpmediadb_presentation class
+	 *
+	 * @access public
+	 * @see phpmediadb_presentation
+	 * @var phpmediadb_presentation
+	 */
+	public $PRESENTATION = null;
+
+	/**
+	 * Container for phpmediadb_business class
+	 *
+	 * @access public
+	 * @see phpmediadb_business
+	 * @var phpmediadb_presentation_session
+	 */
+	public $BUSINESS = null;
+
+	/**
+	 * Container for phpmediadb_data class
+	 *
+	 * @access public
+	 * @see phpmediadb_data
+	 * @var phpmediadb_data
+	 */
+	public $DATA = null;
+
+
+	// --- OPERATIONS ---
+
+	/**
+	 * The constructor __construct initalizes the Class.
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @return void
+	 */
+	public function __construct()
+	{
+		/* assign childs */
+		$this->PRESENTATION		= new phpmediadb_presentation( $this );
+		$this->BUSINESS				= new phpmediadb_business( $this );
+		$this->DATA						= new phpmediadb_data( $this );
+	}
+
+	/**
+	 * The destructor __destruct is responsible for closing all open files,
+	 * etc.
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @return void
+	 */
+	public function __destruct()
+	{
+		/* TODO */
+	}
+
+} /* end of class phpmediadb */
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
