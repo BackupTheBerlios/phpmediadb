b32749e231c13f42e734c0708045c92379b90b68
mblaschke <mblaschke>
2004-09-26 16:06:03 +0000

    * Added basic class * Implemented all Attributes and constructor

--- phpmediadb-cvs/_source/tier.presentation/class.phpmediadb_presentation.php
+++ phpmediadb-cvs/_source/tier.presentation/class.phpmediadb_presentation.php
@@ -1,6 +1,102 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: class.phpmediadb_presentation.php,v 1.1 2004/09/26 15:03:27 mblaschke Exp $ */
+/* $Id: class.phpmediadb_presentation.php,v 1.2 2004/09/26 16:06:03 mblaschke Exp $ */
+
+class phpmediadb_presentation
+{
+	// --- ATTRIBUTES ---
+
+	/**
+	 * Short description of attribute PHPMEDIADB
+	 *
+	 * @access protected
+	 * @see phpmediadb
+	 * @var phpmediadb
+	 */
+	public $PHPMEDIADB = null;
+
+	/**
+	 * Short description of attribute SESSION
+	 *
+	 * @access public
+	 * @see phpmediadb_presentation_session
+	 * @var phpmediadb_presentation_session
+	 */
+	public $SESSION = null;
+
+	/**
+	 * Short description of attribute I18N
+	 *
+	 * @access public
+	 * @see phpmediadb_presentation_i18n
+	 * @var phpmediadb_presentation_i18n
+	 */
+	public $I18N = null;
+
+	/**
+	 * Short description of attribute CONTENTVARS
+	 *
+	 * @access public
+	 * @see phpmediadb_presentation_contentvars
+	 * @var phpmediadb_presentation_contentvars
+	 */
+	public $CONTENTVARS = null;
+
+	/**
+	 * Short description of attribute HTMLSERVICE
+	 *
+	 * @access public
+	 * @see phpmediadb_presentation_htmlservice
+	 * @var phpmediadb_presentation_htmlservice
+	 */
+	public $HTMLSERVICE = null;
+
+	/**
+	 * Short description of attribute XMLSERVICE
+	 *
+	 * @access public
+	 * @see phpmediadb_presentation_xmlservice
+	 * @var phpmediadb_presentation_xmlservice
+	 */
+	public $XMLSERVICE = null;
+
+	// --- OPERATIONS ---
+
+	/**
+	 * The constructor __construct initalizes the Class.
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param phpmediadb
+	 * @return void
+	 */
+	public function __construct( $sender )
+  {
+		/* assign parent */
+		$this->PHPMEDIADB		= $sender;
+
+		/* assign childs */
+		$this->CONTENTVARS	= new phpmediadb_presentation_contentvars( $this );
+		$this->HTMLSERVICE	= new phpmediadb_presentation_htmlservice( $this );
+		$this->I18N					= new phpmediadb_presentation_i18n( $this );
+		$this->SESSION			= new phpmediadb_presentation_session( $this );
+		$this->XMLSERVICE		= new phpmediadb_presentation_xmlservice( $this );
+  }
+
+	/**
+	 * The destructor __destruct is responsible for closing all open files,
+	 * etc.
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @return void
+	 */
+	public function __destruct()
+	{
+		 /* TODO */
+	}
+
+} /* end of class phpmediadb_presentation */
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
