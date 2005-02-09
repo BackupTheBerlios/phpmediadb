9d41e7b413eb9e76256d80df90fcd95c2609d04c
mblaschke <mblaschke>
2005-02-09 21:12:50 +0000

    * Added some configurations

--- phpmediadb-cvs/_source/tier.configuration/config.presentation.php
+++ phpmediadb-cvs/_source/tier.configuration/config.presentation.php
@@ -4,7 +4,29 @@
 
 // $phpMediaDbConfig['PRESENTATION']['example-config'] = "abc";
 
+/*
+ * Generic Configuration
+ */
+$phpMediaDbConfig['PRESENTATION']['generic']['gzip-compression'] = false;
 
+/*
+ * Configuration of directories
+ */
+$phpMediaDbConfig['PRESENTATION']['directory']['root']			= realpath("./") . "/";
+$phpMediaDbConfig['PRESENTATION']['directory']['i18n']			= $phpMediaDbConfig['PRESENTATION']['directory']['root'] . "_source/tier.configuration/i18n/";
+$phpMediaDbConfig['PRESENTATION']['directory']['templates']	= $phpMediaDbConfig['PRESENTATION']['directory']['root'] . "_source/tier.configuration/templates";
+
+/*
+ * Configuration of directories
+ */
+$phpMediaDbConfig['PRESENTATION']['webpath']['server']		= "";
+$phpMediaDbConfig['PRESENTATION']['webpath']['root-path']	= "";
+
+/*
+ * i18n - Configuration
+ */
+$phpMediaDbConfig['PRESENTATION']['i18n']['defaultLanguage']	= "en";
+$phpMediaDbConfig['PRESENTATION']['i18n']['forcedLanguage']		= true;
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
