f3b70ca06c1eaa4c66d01177733db38cd267d4b1
mblaschke <mblaschke>
2005-02-09 21:12:31 +0000

    * Removed unset of global config - didn't work here

--- phpmediadb-cvs/_source/tier.configuration/config.postinit.php
+++ phpmediadb-cvs/_source/tier.configuration/config.postinit.php
@@ -2,10 +2,7 @@
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
 /* $Id $ */
 
-/*
- * Unset phpMediaDbConfig - security
- */
-unset( $phpMediaDbConfig );
+
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
