6e1e98d1645f68f4dbd69cd99ea837c968584d83
mblaschke <mblaschke>
2005-02-09 20:30:47 +0000

    * Initial revision

---
+++ phpmediadb-cvs/_source/tier.global/class.phpmediadb_exception.php
@@ -0,0 +1,22 @@
+<?php
+// phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
+/* $Id: class.phpmediadb_exception.php,v 1.1 2005/02/09 20:30:47 mblaschke Exp $ */
+
+/**
+ * Main class of the phpMediaDB Project. It contains all other classes in the
+ * PRESENTATION, BUSINESS and DATA.
+ *
+ * @access public
+ * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+ * @package phpmediadb
+ * @since 0.0.0
+ * @subpackage global
+ * @version 1.0.0
+ */
+class phpmediadb_exception extends Exception
+{
+	
+} /* end of class phpmediadb_exception */
+
+//--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
+?>
