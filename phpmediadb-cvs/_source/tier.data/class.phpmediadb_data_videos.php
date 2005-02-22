34e9a3be83ccc772204c30888e15b44693f545ea
bruf <bruf>
2005-02-22 15:14:09 +0000

    * Initital revision

---
+++ phpmediadb-cvs/_source/tier.data/class.phpmediadb_data_videos.php
@@ -0,0 +1,58 @@
+<?php
+// phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
+/* $Id: class.phpmediadb_data_videos.php,v 1.1 2005/02/22 15:14:09 bruf Exp $ */
+
+class phpmediadb_data_videos
+{
+	// --- ATTRIBUTES ---
+
+	/**
+	 * Short description of attribute PHPMEDIADB
+	 *
+	 * @access protected
+	 * @see phpmediadb
+	 * @var phpmediadb
+	 */
+	protected $PHPMEDIADB = null;
+
+	/**
+	 * Short description of attribute DATA
+	 *
+	 * @access protected
+	 * @see phpmediadb_presentation
+	 * @var phpmediadb_presentation
+	 */
+	protected $DATA = null;
+
+	// --- OPERATIONS ---
+	
+//-----------------------------------------------------------------------------
+	/**
+	 * The constructor __construct initalizes the Class.
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param phpmediadb_data
+	 * @return void
+	 */
+	public function __construct()
+	{
+		/* nothing to do yet */
+	}
+  
+//-----------------------------------------------------------------------------
+	/**
+	 * The destructor __destruct is responsible for closing all open files,
+	 * etc.
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @return void
+	 */
+	public function __destruct()
+	{
+		/* nothing to do yet */
+	}
+}
+//--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
+?>
