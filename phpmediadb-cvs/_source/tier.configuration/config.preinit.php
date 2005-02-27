b43a02fb808fe7b1c39c96e7a2ebfccad60cb082
mblaschke <mblaschke>
2005-02-27 21:48:44 +0000

    * Added include_path (needed by creole)

--- phpmediadb-cvs/_source/tier.configuration/config.preinit.php
+++ phpmediadb-cvs/_source/tier.configuration/config.preinit.php
@@ -2,10 +2,18 @@
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
 /* $Id $ */
 
-/*
- * Initalize phpMediaDbConfig
+/**
+ * Configuration initalize
  */
 $phpMediaDbConfig = array();
+
+ 
+ /**
+  * Set path - creole need this!
+  */
+	$sourcePath = dirname(dirname(__FILE__) . '..' );
+	ini_set('include_path',  $sourcePath . DIRECTORY_SEPARATOR . 'tier.lib' . PATH_SEPARATOR . ini_get('include_path'));
+
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
