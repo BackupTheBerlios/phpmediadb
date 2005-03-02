a949c61de96578628519f2ca0793a19fb63cdc0d
bruf <bruf>
2005-03-02 15:28:52 +0000

    * Changed some comments * Added method dbConnection

--- phpmediadb-cvs/_source/tier.data/class.phpmediadb_data_sql.php
+++ phpmediadb-cvs/_source/tier.data/class.phpmediadb_data_sql.php
@@ -1,6 +1,6 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: class.phpmediadb_data_sql.php,v 1.1 2005/02/22 16:29:01 bruf Exp $ */
+/* $Id: class.phpmediadb_data_sql.php,v 1.2 2005/03/02 15:28:52 bruf Exp $ */
 
 class phpmediadb_data_sql
 {
@@ -33,7 +33,6 @@ class phpmediadb_data_sql
 	 * @access public
 	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
 	 * @param phpmediadb_data
-	 * @return void
 	 */
 	public function __construct()
 	{
@@ -47,11 +46,25 @@ class phpmediadb_data_sql
 	 *
 	 * @access public
 	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
-	 * @return void
 	 */
 	public function __destruct()
 	{
 		/* nothing to do yet */
+	}
+	
+//-----------------------------------------------------------------------------
+	/**
+	 * This function provide the connection to the database
+	 *
+	 * @access protected
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param String
+	 * @return String
+	*/
+	protected function dbConnection($dns)
+	{
+		$conn = Creole::getConnection($dsn, Creole::PERSISTENT);
+		return $conn;
 	}
 }
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
