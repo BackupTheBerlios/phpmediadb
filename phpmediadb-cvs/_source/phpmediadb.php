416f4726dc260e8d2396f310d81ac2eeb9e5cc57
mblaschke <mblaschke>
2005-03-08 13:37:32 +0000

    * Included all class-files in tier.data

--- phpmediadb-cvs/_source/phpmediadb.php
+++ phpmediadb-cvs/_source/phpmediadb.php
@@ -1,6 +1,6 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: phpmediadb.php,v 1.5 2005/02/27 21:48:19 mblaschke Exp $ */
+/* $Id: phpmediadb.php,v 1.6 2005/03/08 13:37:32 mblaschke Exp $ */
 
 /* run preinit */
 require_once( 'tier.configuration/config.preinit.php' );
@@ -32,6 +32,15 @@ require_once( 'tier.business/class.phpmediadb_business.php' );
 
 /* include tier.data */
 require_once( 'tier.data/class.phpmediadb_data.php' );
+require_once( 'tier.data/class.phpmediadb_data_agerestrictions.php' );
+require_once( 'tier.data/class.phpmediadb_data_audios.php' );
+require_once( 'tier.data/class.phpmediadb_data_categories.php' );
+require_once( 'tier.data/class.phpmediadb_data_codecs.php' );
+require_once( 'tier.data/class.phpmediadb_data_formats.php' );
+require_once( 'tier.data/class.phpmediadb_data_prints.php' );
+require_once( 'tier.data/class.phpmediadb_data_sql.php' );
+require_once( 'tier.data/class.phpmediadb_data_status.php' );
+require_once( 'tier.data/class.phpmediadb_data_videos.php' );
 
 /* run postinit */
 require_once( 'tier.configuration/config.postinit.php' );
