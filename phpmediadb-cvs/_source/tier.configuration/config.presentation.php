7120ab04cb2ef40cf1fbcf1c6b3cf95cb4da38d3
mblaschke <mblaschke>
2005-03-08 17:51:26 +0000

    * Added templates_c as path

--- phpmediadb-cvs/_source/tier.configuration/config.presentation.php
+++ phpmediadb-cvs/_source/tier.configuration/config.presentation.php
@@ -12,21 +12,24 @@ $phpMediaDbConfig['PRESENTATION']['generic']['gzip-compression'] = false;
 /*
  * Configuration of directories
  */
-$phpMediaDbConfig['PRESENTATION']['directory']['root']			= realpath("./") . "/";
-$phpMediaDbConfig['PRESENTATION']['directory']['i18n']			= $phpMediaDbConfig['PRESENTATION']['directory']['root'] . "_source/tier.configuration/i18n/";
-$phpMediaDbConfig['PRESENTATION']['directory']['templates']	= $phpMediaDbConfig['PRESENTATION']['directory']['root'] . "_source/tier.configuration/templates";
+$phpMediaDbConfig['PRESENTATION']['directory']['root']			= realpath('./') . '/';
+$phpMediaDbConfig['PRESENTATION']['directory']['i18n']			= $phpMediaDbConfig['PRESENTATION']['directory']['root'] . '_source/tier.configuration/i18n/';
+$phpMediaDbConfig['PRESENTATION']['directory']['templates']	= $phpMediaDbConfig['PRESENTATION']['directory']['root'] . '_source/tier.configuration/templates';
+$phpMediaDbConfig['PRESENTATION']['directory']['templates_c']	= $phpMediaDbConfig['PRESENTATION']['directory']['root'] . 'var/templates_c';
 
-/*
- * Configuration of directories
- */
-$phpMediaDbConfig['PRESENTATION']['webpath']['server']		= "";
-$phpMediaDbConfig['PRESENTATION']['webpath']['root-path']	= "";
+/**
+  * Configuration of directories
+  */
+$phpMediaDbConfig['PRESENTATION']['webpath']['server']		= '';
+$phpMediaDbConfig['PRESENTATION']['webpath']['root-path']	= '';
 
-/*
- * i18n - Configuration
- */
-$phpMediaDbConfig['PRESENTATION']['i18n']['defaultLanguage']	= "en";
+/**
+  * i18n - Configuration
+  */
+$phpMediaDbConfig['PRESENTATION']['i18n']['defaultLanguage']	= "de";
 $phpMediaDbConfig['PRESENTATION']['i18n']['forcedLanguage']		= true;
+
+
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
