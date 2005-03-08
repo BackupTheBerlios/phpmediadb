5a9530ef63a9ee0b061c770db51ad61855c1ade5
mblaschke <mblaschke>
2005-03-08 17:52:07 +0000

    * Added some i18n variables

--- phpmediadb-cvs/_source/tier.configuration/i18n/i18n.de.php
+++ phpmediadb-cvs/_source/tier.configuration/i18n/i18n.de.php
@@ -4,5 +4,37 @@
 
 // $i18n['STRING-ID'] = "";
 
+/* default i18ns */
+	$i18n['OK'] = 'Akzeptieren';
+	$i18n['CANCEL'] = 'Abbrechen';
+	$i18n['DELETE'] = 'Löschen';
+	$i18n['DELETE'] = 'Löschen';
+	
+	
+/* media data */
+	$i18n['MEDIA_TITLE'] = 'Medientitel';
+	$i18n['MEDIA_ARTIST'] = 'Artist';
+	$i18n['MEDIA_CATEGORY'] = 'Kategorie';
+	
+/* projectdata */
+	$i18n['PROJECT_SUBTITLE'] = 'Die Mediendatenbank';
+	$i18n['PROJECT_LICENSED_UNDER'] = 'Lizenziert unter';
+	$i18n['PROJECT_DESCRIPTION'] = 'Dieses Projekt dient zur Erfassung von auditiven, visuellen und Printmedien. Initiert wurde dieses Projekt von Boris Ruf und Markus Blaschke im Rahmen eines Technikerprojekts an der Werner-Siemens-Schule in Stuttgart.';
+	
+/* Menu items */
+	$i18n['MENU_TITLE'] = 'Navigation';
+	$i18n['MENU_HOME'] = 'Startseite';
+	$i18n['MENU_SEARCH'] = 'Suche';
+	$i18n['MENU_LIST'] = 'Liste';
+	$i18n['MENU_ABOUT'] = 'Über uns';
+
+
+/* Itemtypes */
+	$i18n['ITEM_AUDIO'] = 'Audio';
+	$i18n['ITEM_VIDEO'] = 'Video';
+	$i18n['ITEM_PRINT'] = 'Print';
+
+/* Site: HOME */
+	$i18n['HOME_WELCOME'] = 'Willkommen zu phpMediaDB';
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
