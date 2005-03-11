e968498a2771760d4c4d8aae30ee19c7a9c5fe07
bruf <bruf>
2005-03-11 12:14:09 +0000

    * Added method getFormat and getallFormats

--- phpmediadb-cvs/_source/tier.data/class.phpmediadb_data_formats.php
+++ phpmediadb-cvs/_source/tier.data/class.phpmediadb_data_formats.php
@@ -1,6 +1,6 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: class.phpmediadb_data_formats.php,v 1.3 2005/03/02 15:29:45 bruf Exp $ */
+/* $Id: class.phpmediadb_data_formats.php,v 1.4 2005/03/11 12:14:09 bruf Exp $ */
 
 class phpmediadb_data_formats
 {
@@ -51,6 +51,48 @@ class phpmediadb_data_formats
 	{
 		/* nothing to do yet */
 	}
+//-----------------------------------------------------------------------------
+	/**
+	 * This function returns a specified record from the table MediaFormats
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param Integer
+	 * @return String
+	 */
+	public function getFormat( $MediaFormatID )
+	{
+		$conn = $this->DATA->SQL->getConnection();
+		$stmt = $conn->preparedStatement( 'SELECT *
+		FROM MediaFormats,
+		WHERE MediaFormats.MediaFormatID = :mfid' );
+		$stmt->setString( ':mfid', $MediaFormatID );
+		$stmt->executeQuery();
+		
+		return $stmt;
+	}
+
+//-----------------------------------------------------------------------------
+	/**
+	 * This function returns all records from the table MediaStatus
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @return String
+	 */
+	public function getallStatus()
+	{
+		$conn = $this->DATA->SQL->getConnection();
+		$stmt = $conn->preparedStatement( 'SELECT *
+		FROM MediaFormats,
+		WHERE MediaFormats.MediaFormatID = :mfid' );
+		$stmt->setString( ':mfid', '%' );
+		$stmt->executeQuery();
+		
+		return $stmt;
+	}
+
+//-----------------------------------------------------------------------------
 }
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
