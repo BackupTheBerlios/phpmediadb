2894f54039fe5216ead0f034eb38b3215a073622
bruf <bruf>
2005-03-11 12:17:04 +0000

    * Added method getPrint getallPrints, createPrint, modifyPrint and deletePrint

--- phpmediadb-cvs/_source/tier.data/class.phpmediadb_data_prints.php
+++ phpmediadb-cvs/_source/tier.data/class.phpmediadb_data_prints.php
@@ -1,6 +1,6 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: class.phpmediadb_data_prints.php,v 1.2 2005/03/02 15:29:09 bruf Exp $ */
+/* $Id: class.phpmediadb_data_prints.php,v 1.3 2005/03/11 12:17:04 bruf Exp $ */
 
 class phpmediadb_data_prints
 {
@@ -51,6 +51,215 @@ class phpmediadb_data_prints
 	{
 		/* nothing to do yet */
 	}
+//-----------------------------------------------------------------------------
+	/**
+	 * This function returns a specified record from the table PrintDatas
+	 * and all required data from the other tables
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param Integer
+	 * @return String
+	 */
+	public function getPrint( $PrintDataID )
+	{
+		$conn = $this->DATA->SQL->getConnection();
+		$stmt = $conn->preparedStatement( 'SELECT a.*, b.*, c.*, d.*, e.*, f.*, g.*, h.*, i.ItemPicturesID
+		FROM PrintDatas a,
+		Languages b,
+		Items c,
+		ItemTypes d,
+		MediaCodecs e,
+		MediaFormats f,
+		MediaAgeRestrictions g,
+		MediaStatus h,
+		BinaryDatas i
+		WHERE a.PrintDataID = :pdid 
+		AND a.LanguageID = b.LanguageID
+		AND a.ItemID = c.ItemID
+		AND c.ItemTypeID = d.ItemTypeID
+		AND c.MediaCodecID = e.MediaCodecID
+		AND c.MediaFormatID = f.MediaFormatID
+		AND c.MediaAgeRestrictionID = g.MediaAgeRestrictionID
+		AND c.MediaStatusID = h.MediaStatusID
+		AND c.ItemPicturesID = i.ItemPicturesID' );
+		$stmt->setString( ':pdid', $PrintDataID );
+		$stmt->executeQuery();
+		
+		return $stmt;
+	}
+
+//-----------------------------------------------------------------------------
+	/**
+	 * This function returns all records from the table PrintDatas
+	 * and all required data from the other tables
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @return String
+	 */
+	public function getallPrints()
+	{
+		$conn = $this->DATA->SQL->getConnection();
+		$stmt = $conn->preparedStatement( 'SELECT a.*, b.*, c.*, d.*, e.*, f.*, g.*, h.*, i.ItemPicturesID
+		FROM PrintDatas a,
+		Languages b,
+		Items c,
+		ItemTypes d,
+		MediaCodecs e,
+		MediaFormats f,
+		MediaAgeRestrictions g,
+		MediaStatus h,
+		BinaryDatas i
+		WHERE a.PrintDataID = :pdid 
+		AND a.LanguageID = b.LanguageID
+		AND a.ItemID = c.ItemID
+		AND c.ItemTypeID = d.ItemTypeID
+		AND c.MediaCodecID = e.MediaCodecID
+		AND c.MediaFormatID = f.MediaFormatID
+		AND c.MediaAgeRestrictionID = g.MediaAgeRestrictionID
+		AND c.MediaStatusID = h.MediaStatusID
+		AND c.ItemPicturesID = i.ItemPicturesID' );
+		$stmt->setString( ':pdid', '%' );
+		$stmt->executeQuery();
+		
+		return $stmt;
+	}
+
+//-----------------------------------------------------------------------------
+	/**
+	 * This function delete a specified record from the table PrintDatas
+	 * and all depending data from the other tables
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param Integer
+	 */
+	public function deletePrint( $PrintDataID )
+	{
+		$conn = $this->DATA->SQL->getConnection();
+		$stmt = $conn->preparedStatement( 'DELETE PrintDatas, Items, BinaryDatas, Categories_has_Items
+		FROM PrintDatas, Items, BinaryDatas, Categories_has_Items
+		WHERE PrintDatas.PrintDataID = :pdid
+		AND PrintDatas.ItemID = Items.ItemID
+		AND Items.ItemPicturesID = BinaryDatas.ItemPicturesID
+		AND Items.ItemID = Categories_has_Items.ItemID' );
+		$stmt->setString( ':pdid', $PrintDataID );
+		$stmt->executeUpdate();
+	}
+
+//-----------------------------------------------------------------------------
+/**
+	 * This function create a new record into the table PrintDatas
+	 * and all required data into the other tables
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param Integer
+	 * @param String
+	 * @param String
+	 * @param String
+	 * @param Integer
+	 * @param String
+	 * @param Integer
+	 * @param Integer
+	 * @param Blob
+	 */
+	public function createPrint( $PrintDataMediaCount, $PrintDataISBN, $ItemTitle, $ItemOriginalTitle,
+	$ItemRelease, $ItemMediaName, $ItemCreationDate, $ItemModificationDate, $ItemComment )
+	{
+		$conn = $this->DATA->SQL->getConnection();
+		$stmt = $conn->preparedStatement( 'INSERT INTO PrintDatas ( PrintDataMediaCount, PrintDataISBN ) VALUES( :pdmc, :pdisbn )' );
+		$stmt->setString( ':pdmc', $PrintDataMediaCount );
+		$stmt->setString( ':pdisbn', $PrintDataISBN );
+		$stmt->executeUpdate();
+		
+		$stmt = $conn->preparedStatement( 'INSERT INTO Items
+		( ItemTitle, ItemOriginalTitle, ItemRelease, ItemMediaName, ItemCreationDate, ItemModificationDate, ItemComment )
+		VALUES( :it, :iot, :ir, :imn, :icd, :imd, :ic )' );
+		$stmt->setString( ':it', $ItemTitle );
+		$stmt->setString( ':iot', $ItemOriginalTitle );
+		$stmt->setString( ':ir', $ItemRelease );
+		$stmt->setString( ':imn', $ItemMediaName );
+		$stmt->setString( ':icd', $ItemCreationDate );
+		$stmt->setString( ':imd', $ItemModificationDate );
+		$stmt->setString( ':ic' , $ItemComment );
+		$stmt->executeUpdate(); 
+		
+	}
+
+//-----------------------------------------------------------------------------
+/**
+	 * This function modify a specified record from the table PrintDatas
+	 * and all required data from other tables
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param Integer
+	 * @param String
+	 * @param String
+	 * @param String
+	 * @param Integer
+	 * @param String
+	 * @param Integer
+	 * @param Blob
+	 * @param Integer
+	 */
+	public function modifyPrint( $PrintDataID, $PrintDataMediaCount, $PrintDataISBN, $ItemTitle, $ItemOriginalTitle,
+	$ItemRelease, $ItemMediaName, $ItemModificationDate, $ItemComment, $MediaCodecID, $MediaFormatID,
+	$MediaAgeRestrictionID, $MediaStatusID, $ItemPicturesID )
+	{
+		$conn = $this->DATA->SQL->getConnection();
+		$stmt = $conn->preparedStatement( 'UPDATE PrintDatas,
+		Languages,
+		Items,
+		ItemTypes,
+		MediaCodecs,
+		MediaFormats,
+		MediaAgeRestrictions,
+		MediaStatus,
+		BinaryDatas
+		SET PrintDatas.PrintDataMediaCount = :pdmc,
+		PrintDatas.PrintDataISBN = :pdisbn,
+		Items.ItemTitle = :it,
+		Items.ItemOriginalTitle = :iot,
+		Items.ItemRelease = :ir,
+		Items.ItemMediaName = :imn,
+		Items.ItemModificationDate = :imd,
+		Items.ItemComment = :ic
+		Items.MediaCodecID = :mcid,
+		Items.MediaFormatID = :mfid,
+		Items.MediaAgeRestrictionID = :marid,
+		Items.MediaStatusID = :msid,
+		Items.ItemPicturesID = :ipid
+		WHERE PrintDatas.PrintDataID = :pdid 
+		AND PrintDatas.LanguageID = Languages.LanguageID
+		AND PrintDatas.ItemID = Items.ItemID
+		AND Items.ItemTypeID = ItemTypes.ItemTypeID
+		AND Items.MediaCodecID = MediaCodecs.MediaCodecID
+		AND Items.MediaFormatID = MediaFormats.MediaFormatID
+		AND Items.MediaAgeRestrictionID = MediaAgeRestrictions.MediaAgeRestrictionID
+		AND Items.MediaStatusID = MediaStatus.MediaStatusID
+		AND Items.ItemPicturesID = BinaryDatas.ItemPicturesID' );
+		$stmt->setString( ':pdmc', $PrintDataMediaCount );
+		$stmt->setString( ':pdisbn', $PrintDataISBN );
+		$stmt->setString( ':it', $ItemTitle );
+		$stmt->setString( ':iot', $ItemOriginalTitle );
+		$stmt->setString( ':ir', $ItemRelease );
+		$stmt->setString( ':imn', $ItemMediaName );
+		$stmt->setString( ':imd', $ItemModificatioDate );
+		$stmt->setString( ':ic', $ItemComment );
+		$stmt->setString( ':mcid', $MediaCodecID );
+		$stmt->setString( ':mfid', $MediaFormatID );
+		$stmt->setString( ':marid', $MediaAgeRestrictionID );
+		$stmt->setString( ':msid', $MediaStatusID );
+		$stmt->setString( ':ipid', $ItemPicturesID );
+		$stmt->setString( ':pdid', $PrintDataID );
+		$stmt->executeQuery();
+		
+	}
+
+//-----------------------------------------------------------------------------
 }
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
