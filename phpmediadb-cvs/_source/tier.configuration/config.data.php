40ae9378e3e2085c998ae8ccbbe89678d9d5f62c
mblaschke <mblaschke>
2005-03-14 18:40:32 +0000

    * Added sql-connection configuration

--- phpmediadb-cvs/_source/tier.configuration/config.data.php
+++ phpmediadb-cvs/_source/tier.configuration/config.data.php
@@ -4,5 +4,19 @@
 
 // $phpMediaDbConfig['DATA']['example-config'] = "abc";
 
+/**
+ * SQL Connection
+ *   phptype:   SQL Datbase Type
+ *   hostspec:  Serveraddress (eg. localhost)
+ *   username:  SQL User
+ *   password:  SQL User Password
+ *   database:  SQL Database
+ */
+$phpMediaDbConfig['DATA']['sql-connection']['phptype']  = 'mysql';
+$phpMediaDbConfig['DATA']['sql-connection']['hostspec'] = 'localhost';
+$phpMediaDbConfig['DATA']['sql-connection']['username'] = 'dbuser';
+$phpMediaDbConfig['DATA']['sql-connection']['password'] = 'dbpass';
+$phpMediaDbConfig['DATA']['sql-connection']['database'] = 'phpmediadb';
+
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
