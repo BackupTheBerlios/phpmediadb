7be8dd7b405c960000561638cc06b33d8ba52474
mblaschke <mblaschke>
2005-03-15 21:02:33 +0000

    * Added all check-vars for AUDIOS

--- phpmediadb-cvs/_source/tier.configuration/config.business.php
+++ phpmediadb-cvs/_source/tier.configuration/config.business.php
@@ -8,20 +8,27 @@
  * Itemsepecifications (regex etc.)
  */ 
 
-$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['title'] = '^.{1,255}$';
-$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['originaltitle']  = '^(.{1,255})?$';
-$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['identification']	= '^(.{1,255})?$';
-$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['releaseyear']		= '^([[:digit:]]{4})?$';
-$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['quantity']				= '^([[:digit:]]{0,255})?$';
-$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['mediasize']			= '^(([[:digit:]]+([[:punct:]]{1}[[:digit:]]+)))?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['ItemTitle'] 							= '^.{1,255}$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['ItemOriginalTitle']			= '^(.{1,255})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['ItemMediaName']					= '^(.{1,255})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['ItemIdentification']			= '^(.{1,255})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['ItemRelease']						= '^([[:digit:]]{4})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['Categories']							= '^([[:digit:]]+)?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['ItemMediaSize']					= '^(([[:digit:]]+([[:punct:]]{1}[[:digit:]]+)))?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['MediaFormatID']					= '^([[:digit:]]+)?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['PublisherName']					= '^(.{1,255})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['MediaAgeRestrictionID']	= '^([[:digit:]]+)?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['MediaCodecID']						= '^([[:digit:]]+)?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['ItemQuantity']						= '^(.{1,255})?$';
 
-$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['title'] 					= 255;
-$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['originaltitle'] 	= 255;
-$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['identification'] 	= 255;
-$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['releaseyear']		 	= 4;
-$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['quantity'] 				= 255;
-$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['mediasize'] 			= 255;
-
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['ItemTitle'] 						= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['ItemOriginalTitle']			= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['ItemMediaName']					= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['ItemIdentification']		= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['ItemRelease']						= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['ItemMediaSize']					= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['PublisherName']					= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['ItemQuantity']					= 255;
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
