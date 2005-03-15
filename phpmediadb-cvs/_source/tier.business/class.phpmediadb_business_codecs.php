8dde7c71a5719d3ba7531fb6408fbd6d52bfa81b
mblaschke <mblaschke>
2005-03-15 18:06:26 +0000

    * Initial revision

---
+++ phpmediadb-cvs/_source/tier.business/class.phpmediadb_business_codecs.php
@@ -0,0 +1,76 @@
+<?php
+// phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
+/* $Id: class.phpmediadb_business_codecs.php,v 1.1 2005/03/15 18:06:26 mblaschke Exp $ */
+
+class phpmediadb_business_codecs
+{
+	// --- ATTRIBUTES ---
+
+	/**
+	 * Reference to class phpmediadb
+	 *
+	 * @access protected
+	 * @see phpmediadb
+	 * @var phpmediadb
+	 */
+	protected $PHPMEDIADB = null;
+
+	/**
+	 * Reference to class phpmediadb_business
+	 *
+	 * @access protected
+	 * @see phpmediadb_business
+	 * @var phpmediadb_business
+	 */
+	protected $BUSINESS = null;
+	
+	/**
+	 * Reference to class phpmediadb_data
+	 *
+	 * @access protected
+	 * @see phpmediadb_data
+	 * @var phpmediadb_data
+	 */
+	protected $DATA = null;
+	
+	// --- OPERATIONS ---
+	
+	/**
+	 * The constructor __construct initalizes the Class.
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param phpmediadb
+	 */
+	public function __construct( $sender )
+	{
+		/* init */
+    	 
+		/* assign parent */
+		$this->BUSINESS		= $sender;
+		$this->PHPMEDIADB	= $sender->PHPMEDIADB;
+		$this->DATA				= $sender->PHPMEDIADB->DATA;
+	}
+
+//-----------------------------------------------------------------------------
+	/**
+	 * Returns the complete list
+	 *
+	 *
+	 */
+	public function getList()
+	{
+		/* init */
+		$returnValue = null;
+		
+		/* delegate */
+		$this->DATA->CODECS->getList();
+		
+		/* return data */
+		return $returnValue;
+	}
+	
+} /* end of class phpmediadb_business_codecs */
+
+//--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
+?>
