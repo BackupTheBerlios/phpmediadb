e7d4e5d993b71f9adbbb9ee4089674c0030ac5ae
mblaschke <mblaschke>
2005-03-15 17:39:33 +0000

    * Added input-size and input-regex check configurations

--- phpmediadb-cvs/_source/tier.configuration/config.business.php
+++ phpmediadb-cvs/_source/tier.configuration/config.business.php
@@ -4,5 +4,24 @@
 
 // $phpMediaDbConfig['BUSINESS']['example-config'] = "abc";
 
+/**
+ * Itemsepecifications (regex etc.)
+ */ 
+
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['title'] = '^.{1,255}$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['originaltitle']  = '^(.{1,255})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['identification']	= '^(.{1,255})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['releaseyear']		= '^([[:digit:]]{4})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['quantity']				= '^([[:digit:]]{0,255})?$';
+$phpMediaDbConfig['BUSINESS']['input-regex'][PHPMEDIADB_ITEM_AUDIO]['mediasize']			= '^(([[:digit:]]+([[:punct:]]{1}[[:digit:]]+)))?$';
+
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['title'] 					= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['originaltitle'] 	= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['identification'] 	= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['releaseyear']		 	= 4;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['quantity'] 				= 255;
+$phpMediaDbConfig['BUSINESS']['input-size'][PHPMEDIADB_ITEM_AUDIO]['mediasize'] 			= 255;
+
+
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
 ?>
