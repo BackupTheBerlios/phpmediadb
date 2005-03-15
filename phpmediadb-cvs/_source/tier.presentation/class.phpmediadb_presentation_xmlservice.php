37e0a449f63932031cb5d75ea7d20a64271f8741
mblaschke <mblaschke>
2005-03-15 20:25:42 +0000

    * Added basic classlayout

--- phpmediadb-cvs/_source/tier.presentation/class.phpmediadb_presentation_xmlservice.php
+++ phpmediadb-cvs/_source/tier.presentation/class.phpmediadb_presentation_xmlservice.php
@@ -1,17 +1,45 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: class.phpmediadb_presentation_xmlservice.php,v 1.2 2004/09/26 16:04:22 mblaschke Exp $ */
+/* $Id: class.phpmediadb_presentation_xmlservice.php,v 1.3 2005/03/15 20:25:42 mblaschke Exp $ */
 
 class phpmediadb_presentation_xmlservice
 {
+	// --- ATTRIBUTES ---
 
+	/**
+	 * Reference to class phpmediadb
+	 *
+	 * @access protected
+	 * @see phpmediadb
+	 * @var phpmediadb
+	 */
+	protected $PHPMEDIADB = null;
 
+	/**
+	 * Reference to class phpmediadb_presentation
+	 *
+	 * @access protected
+	 * @see phpmediadb_presentation
+	 * @var phpmediadb_presentation
+	 */
+	protected $PRESENTATION = null;
+
+//-----------------------------------------------------------------------------
+	/**
+	 * The constructor __construct initalizes the Class.
+	 *
+	 * @access public
+	 * @author phpMediaDB Team - http://phpmediadb.berlios.de/
+	 * @param phpmediadb_presentation $sender Reference to parent class
+	 */
 	public function __construct( $sender )
 	{
-    	 /* TODO */
-	}
+		/* assign parent */
+		$this->PRESENTATION	= $sender;
+		$this->PHPMEDIADB		= $sender->PHPMEDIADB;
+  }
 
-
+//-----------------------------------------------------------------------------
 } /* end of class phpmediadb_presentation_xmlservice */
 
 //--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
