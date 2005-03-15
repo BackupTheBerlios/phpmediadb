7fbaef10468a01022a027af7aeac533ade120ed1
mblaschke <mblaschke>
2005-03-15 18:06:52 +0000

    * Included all other tier.business classes

--- phpmediadb-cvs/_source/tier.business/class.phpmediadb_business.php
+++ phpmediadb-cvs/_source/tier.business/class.phpmediadb_business.php
@@ -1,6 +1,6 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: class.phpmediadb_business.php,v 1.5 2005/02/27 16:03:16 mblaschke Exp $ */
+/* $Id: class.phpmediadb_business.php,v 1.6 2005/03/15 18:06:52 mblaschke Exp $ */
 
 class phpmediadb_business
 {
@@ -9,11 +9,20 @@ class phpmediadb_business
 	/**
 	 * Reference to class phpmediadb
 	 *
-	 * @access protected
+	 * @access public
 	 * @see phpmediadb
 	 * @var phpmediadb
 	 */
 	public $PHPMEDIADB = null;
+	
+	public $AGERESTRICTIONS = null;
+	public $AUDIOS = null;
+	public $CATEGORIES = null;
+	public $CODECS = null;
+	public $FORMATS = null;
+	public $INSPECTOR = null;
+	public $PRINTS = null;
+	public $VIDEOS = null;
 
 	/**
 	 * Container of configuration for the whole tier
@@ -24,7 +33,8 @@ class phpmediadb_business
 	public $configuration = null;
 	
 	// --- OPERATIONS ---
-	
+
+//-----------------------------------------------------------------------------	
 	/**
 	 * The constructor __construct initalizes the Class.
 	 *
@@ -41,8 +51,19 @@ class phpmediadb_business
     	 
 		/* assign parent */
 		$this->PHPMEDIADB		= $sender;
+		
+		/* assign childs */
+		$this->AGERESTRICTIONS	= new phpmediadb_business_agerestrictions( $this );
+		$this->AUDIOS						= new phpmediadb_business_audios( $this );
+		$this->CATEGORIES				= new phpmediadb_business_categories( $this );
+		$this->CODECS						= new phpmediadb_business_codecs( $this );
+		$this->FORMATS					= new phpmediadb_business_formats( $this );
+		$this->INSPECTOR				= new phpmediadb_business_inspector( $this );
+		$this->PRINTS						= new phpmediadb_business_prints( $this );
+		$this->VIDEOS						= new phpmediadb_business_prints( $this );
 	}
 
+//-----------------------------------------------------------------------------
 	/**
 	 * Loads the configuration from the global array and resets the array
 	 *
