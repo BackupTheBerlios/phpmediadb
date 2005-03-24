2a1187a04a41140c822a13c9e7eebd0c85d1323c
mblaschke <mblaschke>
2005-03-24 20:44:31 +0000

    * Added PHPMEDIADB_ITEMINFO_AGERESTRICTION, PHPMEDIADB_ITEMINFO_CATEGORIES, PHPMEDIADB_ITEMINFO_CODECS, PHPMEDIADB_ITEMINFO_FORMATS and PHPMEDIADB_ITEMINFO_STATUS

--- phpmediadb-cvs/_source/tier.global/constants.phpmediadb.php
+++ phpmediadb-cvs/_source/tier.global/constants.phpmediadb.php
@@ -1,11 +1,11 @@
 <?php
 // phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
-/* $Id: constants.phpmediadb.php,v 1.4 2005/03/24 17:12:48 mblaschke Exp $ */
+/* $Id: constants.phpmediadb.php,v 1.5 2005/03/24 20:44:31 mblaschke Exp $ */
 /**
  * This files handels all contants
  * 
  * @author		Markus Blaschke <mblaschke@users.berlios.de>
- * @version		$Revision: 1.4 $
+ * @version		$Revision: 1.5 $
  * @package		phpmediadb
  * @subpackage	global
  */
@@ -16,6 +16,7 @@
      * Constant that indicates that the content is already HTML
      */
 	define( 'PHPMEDIADB_NODEFORMAT_HTML', 1 );
+	
     /**
      * Constant that indicates that the content is only text
      * and should be converted. XML/XHTML ruleset is used.
@@ -26,15 +27,42 @@
     /**
      * Constant that indicated that the item is an audio-media
      */
+     
 	define( 'PHPMEDIADB_ITEM_AUDIO', 1 );
     /**
      * Constant that indicated that the item is an video-media
      */
 	define( 'PHPMEDIADB_ITEM_VIDEO', 2 );
+	
     /**
      * Constant that indicated that the item is an print-media
      */
 	define( 'PHPMEDIADB_ITEM_PRINT', 3 );
+	
+    /**
+     * Constant that indicated that the iteminfo is a agerestriction-info
+     */	
+	define( 'PHPMEDIADB_ITEMINFO_AGERESTRICTION', 900 );
+
+    /**
+     * Constant that indicated that the iteminfo is a category
+     */	
+	define( 'PHPMEDIADB_ITEMINFO_CATEGORIES', 901 );
+	
+    /**
+     * Constant that indicated that the iteminfo is a codec
+     */	
+	define( 'PHPMEDIADB_ITEMINFO_CODECS', 902 );
+	
+    /**
+     * Constant that indicated that the iteminfo is a format
+     */	
+	define( 'PHPMEDIADB_ITEMINFO_FORMATS', 903 );
+	
+    /**
+     * Constant that indicated that the iteminfo is a status
+     */	
+	define( 'PHPMEDIADB_ITEMINFO_STATUS', 904 );
 
 // --- tier.data ---
 
