f72b7b34e8a85088c870caf51d6dc314f921899e
mblaschke <mblaschke>
2005-04-12 18:48:33 +0000

    * Initial revision

---
+++ phpmediadb-cvs/item-details.php
@@ -0,0 +1,20 @@
+<?php
+// phpMediaDB :: Licensed under GNU-GPL :: http://phpmediadb.berlios.de/
+/* $Id: item-details.php,v 1.1 2005/04/12 18:48:33 mblaschke Exp $ */
+/**
+ * This file displays all details of an item
+ * 
+ * @author		Markus Blaschke <mblaschke@users.berlios.de>
+ * @version		$Revision: 1.1 $
+ * @package		phpmediadb_html
+ * @subpackage	access_anonymous
+ */
+ 
+/* include main phpmediadb-project */
+require_once( '_source/phpmediadb.php' );
+
+/* create object */
+$PHPMEDIADB	= new phpmediadb(); 
+
+//--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
+?>
