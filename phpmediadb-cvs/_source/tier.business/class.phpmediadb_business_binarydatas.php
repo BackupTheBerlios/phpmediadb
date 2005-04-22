dab1a5ed30a803deac861bd2064082b325861dd3
mblaschke <mblaschke>
2005-04-22 21:54:32 +0000

    * Initial revision

---
+++ phpmediadb-cvs/_source/tier.business/class.phpmediadb_business_binarydatas.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * $Id: class.phpmediadb_business_binarydatas.php,v 1.1 2005/04/22 21:54:32 mblaschke Exp $
+ *
+ * Project:     phpMediaDB :: OpenSource Mediadatabase
+ * File:        class.phpmediadb_business_binarydatas.php
+ * License:     GNU General Publice License
+ *
+ * This file is part of phpMediaDB.
+ *
+ * phpMediaDB is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For questions, help, comments, discussion, etc., please join the
+ * phpMediaDB mailing list. See phpMediaDB projectportal for more
+ * information.
+ *
+ * @link        http://phpmediadb.berlios.de/
+ * @copyright   2004-2005 &copy; Blaschke, Markus; Ruf, Boris
+ * @license     http://opensource.org/licenses/gpl-license.php GNU General Public License
+ * @author      Markus Blaschke <mblaschke@users.berlios.de>
+ * @author      Boris Ruf <bruf@users.berlios.de>
+ * @package		phpmediadb
+ * @subpackage	business
+ * @version     $Revision: 1.1 $
+ */
+
+/**
+ * This is the class that manages all functions of the binarydatas
+ */
+class phpmediadb_business_binarydatas
+{
+	// --- ATTRIBUTES ---
+
+	/**
+	 * Reference to class phpmediadb
+	 *
+	 * @access protected
+	 * @see phpmediadb
+	 * @var phpmediadb
+	 */
+	protected $PHPMEDIADB = null;
+
+	/**
+	 * Reference to class phpmediadb_business
+	 *
+	 * @access protected
+	 * @see phpmediadb_business
+	 * @var phpmediadb_business
+	 */
+	protected $BUSINESS = null;
+	
+	/**
+	 * Reference to class phpmediadb_data
+	 *
+	 * @access protected
+	 * @see phpmediadb_data
+	 * @var phpmediadb_data
+	 */
+	protected $DATA = null;
+	
+	// --- OPERATIONS ---
+	
+//-----------------------------------------------------------------------------
+	/**
+	 * The constructor __construct initalizes the Class.
+	 *
+	 * @access public
+	 * @param phpmediadb_business $sender Reference to parent class
+	 */
+	public function __construct( $sender )
+	{
+		/* init */
+    	 
+		/* assign parent */
+		$this->BUSINESS		= $sender;
+		$this->PHPMEDIADB	= $sender->PHPMEDIADB;
+		$this->DATA			= $sender->PHPMEDIADB->DATA;
+	}
+	
+//-----------------------------------------------------------------------------
+} /* end of class phpmediadb_business_binarydatas */
+
+//--- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF --- EOF ---
+?>
